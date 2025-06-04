0a4fefb1a742d91caee26160a826c4bed61e6ce6
m4md24 <m4md24@gmail.com>
2025-06-04 17:44:19 +0300

    Learn (WriteLine) Concept

---
+++ src/_1_concepts/_1_5_syntax/_1_5_1_console_app/_1_5_1_1_output/_1_5_1_1_1_write_line/WriteLineExample.cs
@@ -0,0 +1,23 @@
+namespace CS_Level_1_by_Programming_Advices._1_concepts._1_5_syntax._1_5_1_console_app._1_5_1_1_output._1_5_1_1_1_write_line;
+
+internal class WriteLineExample {
+    public static void Main(
+        string[] args
+    ) {
+        Console.WriteLine(
+            "Hello Developers!"
+        );
+        Console.WriteLine(
+            "My name is Mohamed Sadawy."
+        );
+        Console.WriteLine(
+            "I learned C# and I am currently re-learning it."
+        );
+        Console.WriteLine(
+            "EX1: The Sum of 10 + 20 is: " + (10 + 20)
+        );
+        Console.WriteLine(
+            "EX2: The Sum of 10 + 20 is: " + 10 + 20 // With Concatenating
+        );
+    }
+}
