822205998badb8822a04913b928734a63dba8a03
m4md24 <m4md24@gmail.com>
2025-06-04 19:09:23 +0300

    Learn (Comments) Concept

--- src/_1_concepts/_1_5_syntax/_1_5_1_console_app/_1_5_1_1_output/_1_5_1_1_4_escape_characters/EscapeCharacters.cs
+++ src/_1_concepts/_1_5_syntax/_1_5_1_console_app/_1_5_1_1_output/_1_5_1_1_4_escape_characters/EscapeCharacters.cs
@@ -3,7 +3,7 @@ using System;
 namespace CS_Level_1_by_Programming_Advices._1_concepts._1_5_syntax._1_5_1_console_app._1_5_1_1_output._1_5_1_1_4_escape_characters;
 
 internal class EscapeCharacters {
-    public static void Main(
+    public static void main(
         string[] args
     ) {
         Console.WriteLine(
---
+++ src/_1_concepts/_1_5_syntax/_1_5_1_console_app/_1_5_1_2_comments/WriteLineExample.cs
@@ -0,0 +1,13 @@
+namespace CS_Level_1_by_Programming_Advices._1_concepts._1_5_syntax._1_5_1_console_app._1_5_1_2_comments;
+
+internal class Comments {
+    public static void Main(
+        string[] args
+    ) {
+        // Single Comment
+
+        /*
+         * Multiple Comment
+         */
+    }
+}
