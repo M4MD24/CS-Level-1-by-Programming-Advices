6072997958c50c83825b8a469a3eefc2ba329ccf
m4md24 <m4md24@gmail.com>
2025-06-12 22:18:44 +0300

    Initialize UI

--- WindowsFormsProjects/TicTacToe/Main.Designer.cs
+++ WindowsFormsProjects/TicTacToe/Main.Designer.cs
@@ -29,11 +29,16 @@ partial class Main {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-        title = new System.Windows.Forms.Label();
+        title           = new System.Windows.Forms.Label();
+        game            = new System.Windows.Forms.GroupBox();
+        round           = new System.Windows.Forms.GroupBox();
+        statistics      = new System.Windows.Forms.GroupBox();
+        resetStatistics = new System.Windows.Forms.Button();
+        resetRound      = new System.Windows.Forms.Button();
         SuspendLayout();
-        // 
+        //
         // title
-        // 
+        //
         title.Font = new System.Drawing.Font(
             "Kristen ITC",
             27F,
@@ -42,7 +47,7 @@ partial class Main {
             ((byte) 0)
         );
         title.Location = new System.Drawing.Point(
-            375,
+            225,
             25
         );
         title.Name = "title";
@@ -53,17 +58,119 @@ partial class Main {
         title.TabIndex  = 0;
         title.Text      = "Tic Tac Toe";
         title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
+        //
+        // game
+        //
+        game.Font = new System.Drawing.Font(
+            "Segoe UI",
+            12F
+        );
+        game.Location = new System.Drawing.Point(
+            250,
+            125
+        );
+        game.Name = "game";
+        game.Size = new System.Drawing.Size(
+            400,
+            400
+        );
+        game.TabIndex = 5;
+        game.TabStop  = false;
+        game.Text     = "Game";
+        //
+        // round
+        //
+        round.Font = new System.Drawing.Font(
+            "Segoe UI",
+            12F
+        );
+        round.Location = new System.Drawing.Point(
+            50,
+            125
+        );
+        round.Name = "round";
+        round.Size = new System.Drawing.Size(
+            150,
+            150
+        );
+        round.TabIndex = 1;
+        round.TabStop  = false;
+        round.Text     = "Round 1";
+        //
+        // statistics
+        //
+        statistics.Font = new System.Drawing.Font(
+            "Segoe UI",
+            12F
+        );
+        statistics.Location = new System.Drawing.Point(
+            50,
+            330
+        );
+        statistics.Name = "statistics";
+        statistics.Size = new System.Drawing.Size(
+            150,
+            150
+        );
+        statistics.TabIndex = 3;
+        statistics.TabStop  = false;
+        statistics.Text     = "Statistics";
+        //
+        // resetStatistics
+        //
+        resetStatistics.Location = new System.Drawing.Point(
+            75,
+            495
+        );
+        resetStatistics.Name = "resetStatistics";
+        resetStatistics.Size = new System.Drawing.Size(
+            100,
+            25
+        );
+        resetStatistics.TabIndex                = 4;
+        resetStatistics.Text                    = "Reset Statistics";
+        resetStatistics.UseVisualStyleBackColor = true;
+        //
+        // resetRound
+        //
+        resetRound.Location = new System.Drawing.Point(
+            75,
+            290
+        );
+        resetRound.Name = "resetRound";
+        resetRound.Size = new System.Drawing.Size(
+            100,
+            25
+        );
+        resetRound.TabIndex                = 2;
+        resetRound.Text                    = "Reset Round";
+        resetRound.UseVisualStyleBackColor = true;
+        //
         // Main
-        // 
+        //
         AutoScaleDimensions = new System.Drawing.SizeF(
             7F,
             15F
         );
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(
-            984,
+            684,
             561
+        );
+        Controls.Add(
+            resetStatistics
+        );
+        Controls.Add(
+            resetRound
+        );
+        Controls.Add(
+            statistics
+        );
+        Controls.Add(
+            round
+        );
+        Controls.Add(
+            game
         );
         Controls.Add(
             title
@@ -75,6 +182,16 @@ partial class Main {
             false
         );
     }
+
+    private System.Windows.Forms.Button resetRound;
+
+    private System.Windows.Forms.Button resetStatistics;
+
+    private System.Windows.Forms.GroupBox statistics;
+
+    private System.Windows.Forms.GroupBox round;
+
+    private System.Windows.Forms.GroupBox game;
 
     private System.Windows.Forms.Label title;
 
